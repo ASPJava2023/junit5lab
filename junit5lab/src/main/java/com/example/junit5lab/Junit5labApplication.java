1f6dfb6eb8e5cb9872643579ee9a4eaf7ba8db63
ASPJava2023 <ankit.singh.pawar.newlaptop@gmail.com>
2025-08-28 15:24:09 +0530

    Basic Project

---
+++ junit5lab/src/main/java/com/example/junit5lab/Junit5labApplication.java
@@ -0,0 +1,13 @@
+package com.example.junit5lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5labApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Junit5labApplication.class, args);
+	}
+
+}
